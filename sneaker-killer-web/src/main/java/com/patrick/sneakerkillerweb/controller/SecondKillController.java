e0fc8c308b5c2c6d5be7b0af411e7c50f258d918
周健 <patrick@zhoujiandeMacBook-Pro.local>
2021-01-16 09:41:48 +0800

    add redis

--- sneaker-killer-web/src/main/java/com/patrick/sneakerkillerweb/controller/SecondKillController.java
+++ sneaker-killer-web/src/main/java/com/patrick/sneakerkillerweb/controller/SecondKillController.java
@@ -6,6 +6,7 @@ import com.patrick.sneakerkillerservice.util.JWTUtil;
 import com.patrick.sneakerkillerweb.result.Result;
 import com.patrick.sneakerkillerweb.result.ResultFactory;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(value = "/api")
 public class SecondKillController {
 
     SendMessageService sendMessageService;
+    StringRedisTemplate redisTemplate;
 
-    public SecondKillController(SendMessageService sendMessageService){
+    public SecondKillController(SendMessageService sendMessageService, StringRedisTemplate redisTemplate){
         this.sendMessageService = sendMessageService;
+        this.redisTemplate = redisTemplate;
     }
 
     @PostMapping(value = "/item/order")
@@ -34,6 +38,13 @@ public class SecondKillController {
         String token = request.getHeader("Authorization");
         Integer userId = JWTUtil.getUserId(token);
         dto.setUserId(userId);
+        String redisKey = new StringBuffer().append(userId).append(dto.getItemId()).append("-redis").toString();
+        // 先看缓存有没有信息，有说明用户已经提交过请求了，直接返回
+        if(redisTemplate.opsForValue().get(redisKey) != null){
+            return ResultFactory.buildFailResult("请勿重复提交");
+        }
+        // 没有就在缓存中创建一个kv，过期时间为30分钟
+        redisTemplate.opsForValue().set(redisKey, String.valueOf(userId), 30, TimeUnit.MINUTES);
         sendMessageService.sendMessageToMQ(dto);
         return ResultFactory.buildSuccessResult("订单已提交");
     }
--- sneaker-killer-web/src/test/java/com/patrick/sneakerkillerweb/SneakerKillerWebApplicationTests.java
+++ sneaker-killer-web/src/test/java/com/patrick/sneakerkillerweb/SneakerKillerWebApplicationTests.java
@@ -5,11 +5,16 @@ import com.patrick.sneakerkillerservice.config.PropertiesConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 
 @SpringBootTest
 class SneakerKillerWebApplicationTests {
     @Test
     void contextLoads() {
+
     }
+
 }
